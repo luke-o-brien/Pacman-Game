7df113abb034668fca3218bcc9ad356abb7b5856
Luke O'Brien <lukeobrien02@gmail.com>
2022-04-12 22:18:44 +0100

    updated menu and game with working vulnerable ghosts

--- script/app.js
+++ script/app.js
@@ -2,7 +2,7 @@
 const start = document.querySelector('.start-menu')
 const main = document.querySelector('main')
 const startButton = document.querySelector('.play-button')
-
+const howToplay = document.querySelector('.how-to-play')
 
 
 startButton.addEventListener('click', () => {
@@ -24,13 +24,13 @@ function createlayout() {
 }
 
 
-
-
 //Creation of map layout 
+
 const layout = [
   'w' , 'w' , 'w' , 'w', 'w', 'w', 'w', 'w', 
   'w' , 'p' , 'p' , 'p', 'p', 'p', 'p', 'w', 
   'w' , 'p' , 'p' , 'p', 'p', 'p', 'p', 'w', 
+  'w' , 'p' , 'p' , 'p', 'p', 'p', 'p', 'w',
   'w' , 'p' , 'p' , 'p', 'p', 'p', 'p', 'w',
   'w' , 'p' , 'p' , 'p', 'p', 'p', 'P', 'w',
   'w' , 'w' , 'w' , 'w', 'w', 'w', 'w', 'w']
@@ -42,24 +42,43 @@ const dotArray = []
 let pacmanPosition = 27;
 let ghostposition = 11;
 let points = 0;
+let highScore = 0
+let ghostVulnerable = false
 
 
 //Pacman Spawn and Delete Functions 
 function spawnPacman() {
-  map[pacmanPosition].classList.add('pacman')
+  const spawnSquare = document.getElementById(pacmanPosition)
+  const createPacman = document.createElement("div")
+  createPacman.classList = 'pacman' 
+  createPacman.id = 'pacman'
+  spawnSquare.appendChild(createPacman)
 }
 
 function deletePacman() {
-  map[pacmanPosition].classList.remove('pacman')
+  const deleteFromSquare = document.getElementById(pacmanPosition)
+  const pacman = deleteFromSquare.querySelector('.pacman')
+  deleteFromSquare.removeChild(pacman)
+
 }
 
 //Ghost Spawn and Delete Functions
 function spawnGhost() {
-  map[ghostposition].classList.add('ghost')
+  const ghostspawnSquare = document.getElementById(ghostposition)
+  const createGhost = document.createElement("div")
+  createGhost.id = 'ghost'
+  if (ghostVulnerable === true) {
+    createGhost.classList = 'ghost-vulnerable'
+  } else if (ghostVulnerable === false) {
+    createGhost.classList = 'ghost' 
+  }
+  ghostspawnSquare.appendChild(createGhost)
 }
 
 function deleteGhost() {
-  map[ghostposition].classList.remove('ghost')
+  const deleteGhostSquare = document.getElementById(ghostposition)
+  const ghostremoval = deleteGhostSquare.querySelector('#ghost')
+  deleteGhostSquare.removeChild(ghostremoval)
 }
 
 const game = document.querySelector('.game')
@@ -136,21 +155,21 @@ function ghostMoves() {
     const ghostOptions = ['up', 'down', 'left', 'right']
     const ghostChoice =  ghostOptions[Math.floor(Math.random() * ghostOptions.length)]
     if (ghostChoice === 'right' && map[ghostposition + 1].classList.contains('path')) {
-      map[ghostposition].classList.remove('ghost')
+      deleteGhost(ghostposition)
       ghostposition += 1 
-      map[ghostposition].classList.add('ghost')
+      spawnGhost(ghostposition)
     } else if ( ghostChoice === 'down' && map[ghostposition + 8].classList.contains('path')) {
-      map[ghostposition].classList.remove('ghost')
+      deleteGhost(ghostposition)
       ghostposition += 8
-      map[ghostposition].classList.add('ghost')
+      spawnGhost(ghostposition)
     } else if ( ghostChoice === 'left' && map[ghostposition - 1].classList.contains('path')) {
-      map[ghostposition].classList.remove('ghost')
+      deleteGhost(ghostposition)
       ghostposition -= 1
-      map[ghostposition].classList.add('ghost')
+      spawnGhost(ghostposition)
     } else if ( ghostChoice === 'up' && map[ghostposition - 8].classList.contains('path')) {
-      map[ghostposition].classList.remove('ghost')
+      deleteGhost(ghostposition)
       ghostposition -= 8
-      map[ghostposition].classList.add('ghost')
+      spawnGhost(ghostposition)
     }
     GhostKillsPacman()
   }, 1000)
@@ -176,6 +195,8 @@ function pacmanEats() {
     active.removeChild(bigdots)
     points += 15
     dotArray.splice(bigDotIndex, 1)
+    vulnerableGhosts()
+    console.log(ghostVulnerable)
     if (dotArray.length === 0) {
       console.log('you have won')
       WinnerScreen()
@@ -183,14 +204,59 @@ function pacmanEats() {
   } else if (map[pacmanPosition].children[0].id === null) {
     console.log('no dot')
   }
-  console.log(dotArray)
 }
 
+//Pacman eats big dot and ghosts become vulnerable
+function vulnerableGhosts() {
+  ghostVulnerable = true 
+  
+  const timeout = setTimeout(resetVulstatus, 5000);
+
+  function resetVulstatus() {
+    ghostVulnerable = false
+  }
+}
 
 // ghost and pacman meet ending the game and displaying points and option to restart game 
 function GhostKillsPacman() {
-  if (map[pacmanPosition] === map[ghostposition]) {
-    WinnerScreen()
+  if (map[pacmanPosition] === map[ghostposition] && ghostVulnerable === false) {
+    main.innerHTML = ''
+    const gameOver = document.createElement("div");
+    main.appendChild(gameOver)
+    gameOver.classList.add('game-over')
+    const results = document.createElement('p')
+    results.innerHTML = 'Better Luck next time'
+    results.classList.add('results')
+
+    const finalPoints = document.createElement('p')
+    finalPoints.innerHTML = points + ' points'
+    finalPoints.classList.add('final-points')
+  
+  
+    const replay = document.createElement('button')
+    replay.innerHTML = 'click to replay'
+    replay.classList.add('replay-button')
+
+    gameOver.appendChild(results)
+    gameOver.appendChild(finalPoints)
+    gameOver.appendChild(replay)
+
+
+    const replayButton = document.querySelector('.replay-button')
+
+    replayButton.addEventListener('click', () => {
+      console.log('replay clicked')
+
+      main.innerHTML = ''
+    
+      console.log(document)
+      createlayout()
+    })
+    
+  } else if (map[pacmanPosition] === map[ghostposition] && ghostVulnerable === true) {
+    deleteGhost(ghostposition)
+    ghostposition = 11
+    spawnGhost(ghostposition)
   }
 }
 
@@ -218,14 +284,29 @@ function WinnerScreen() {
   gameOver.appendChild(finalPoints)
   gameOver.appendChild(replay)
 
+
   const replayButton = document.querySelector('.replay-button')
 
   replayButton.addEventListener('click', () => {
     console.log('replay clicked')
+
     main.innerHTML = ''
+    
     console.log(document)
     createlayout()
-
-
-})
+  })
 }
+
+// CODE FOR OTHER MENUS 
+
+howToplay.addEventListener('click', () => {
+  console.log('how to play clicked')
+  main.innerHTML = ''
+  const htpPage = document.createElement("div");
+  htpPage.classList = 'htp-page'
+  main.appendChild(htpPage)
+  const title = document.createElement("h2")
+  title.classList = 'htp-h2'
+  title.innerHTML = 'How to Play'
+  htpPage.appendChild(title)
+})
